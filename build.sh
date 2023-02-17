58cf13fcdc92f202d8eb90c270f4555e7bc5a204
Your Name <you@example.com>
2023-02-17 11:18:00 +0100

    organize lineage_build_unified and lineage_patches_unified changes into a fork

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 set -xe
 
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt install -y zstd
+
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 
@@ -9,32 +13,10 @@ then
 	(
 		cd src
 		repo init -u https://github.com/droidng/android.git -b ng-v3
-		git clone https://github.com/AndyCGYan/lineage_build_unified lineage_build_unified -b lineage-19.1
-		git clone https://github.com/AndyCGYan/lineage_patches_unified lineage_patches_unified -b lineage-19.1
-		# duplicated gitlab remote
-		sed -i 's#<remote name="gitlab" fetch="https://gitlab.com/" />##g' lineage_build_unified/local_manifests_treble/manifest.xml
-		# droid-ng has their own gapps repo
-		sed -i 's#<project name="davi.sh/gms-android-12" path="vendor/partner_gms" remote="gitlab" revision="master" />##g' lineage_build_unified/local_manifests_treble/manifest.xml
+		git clone https://github.com/Kethen/lineage_build_unified lineage_build_unified -b ng-v3
+		git clone https://github.com/Kethen/lineage_patches_unified lineage_patches_unified -b ng-v3
 		# un-clonable without access
 		sed -i 's#<project name="droid-ng/android_vendor_priv" path="vendor/priv" remote="private" revision="master" groups="notdefault,ng_priv" />##g' .repo/manifests/snippets/ng.xml
-		# don't do repopick for droid-ng
-		sed -i '/repopick/d' lineage_build_unified/buildbot_unified.sh
-		# these patch are already in droid-ng
-		rm lineage_patches_unified/patches_treble_phh/platform_frameworks_base/0004-Show-APN-Settings-for-CDMA-carriers.patch
-		rm lineage_patches_unified/patches_treble_phh/platform_frameworks_base/0009-HOME-deserves-to-wake-up-devices-just-as-well-as-bac.patch
-		rm lineage_patches_unified/patches_treble_phh/platform_frameworks_base/0010-Some-devices-have-proximity-sensor-reporting-NaN-as-.patch
-		rm lineage_patches_unified/patches_treble_phh/platform_frameworks_base/0018-Fix-Wakelock-issue.patch
-		rm lineage_patches_unified/patches_treble_phh/platform_frameworks_base/0022-Add-support-for-app-signature-spoofing.patch
-		rm lineage_patches_unified/patches_treble_phh/platform_system_netd/0001-Support-no-bpf-usecase.patch
-		rm lineage_patches_unified/patches_treble_phh/platform_system_bpf/0001-Support-no-bpf-usecase.patch
-		rm lineage_patches_unified/patches_treble/system_core/0002-Panic-into-recovery-rather-than-bootloader.patch
-		rm lineage_patches_unified/patches_treble_phh/platform_frameworks_opt_net_ims/0001-Pie-MTK-IMS-calls-static-ImsManager.updateImsService.patch
-		rm lineage_patches_unified/patches_treble_phh/platform_frameworks_opt_telephony/0004-Reintroduce-public-void-TelephonyMetrics.writeRilSen.patch
-
-
-		# keep that droid-ng style
-		rm -r lineage_patches_unified/patches_platform/*
-
 	)
 fi
 
@@ -53,9 +35,8 @@ then
 	bash lineage_build_unified/buildbot_unified.sh treble 64VN
 	touch synchronized
 else
-	bash lineage_build_unified/buildbot_unified.sh treble nosync 64VN
+	bash lineage_build_unified/buildbot_unified.sh treble nosync A64VN A64VS 64VN 64VS
 fi
-
 
 #bash -l
 #exit 0
--- build_podman.sh
+++ build_podman.sh
@@ -1,12 +1,14 @@
 set -xe
 
-mkdir -p workdir
+mkdir -p workdir homedir
 
 podman run \
 	--rm -it \
 	--security-opt label=disable \
 	-v ./workdir:/workdir \
 	-w /workdir \
+	-v ./homedir:/homedir \
+	-e HOME=/homedir \
 	-v ./build.sh:/build.sh \
 	--entrypoint '["/bin/bash", "/build.sh"]' \
 	lineageos4microg/docker-lineage-cicd
