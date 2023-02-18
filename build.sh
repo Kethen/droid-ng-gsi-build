5ef1774e67c5af7dd7ad03cdd6583abf9afe9715
Your Name <you@example.com>
2023-02-18 08:41:08 +0100

    build more targets

--- build.sh
+++ build.sh
@@ -32,10 +32,10 @@ cd src
 #if ! [ -e synchronized ]
 if true
 then
-	bash lineage_build_unified/buildbot_unified.sh treble 64VN
+	bash lineage_build_unified/buildbot_unified.sh treble 64VN A64VN 64VS A64VS
 	touch synchronized
 else
-	bash lineage_build_unified/buildbot_unified.sh treble nosync A64VN A64VS 64VN 64VS
+	bash lineage_build_unified/buildbot_unified.sh treble nosync 64VN A64VN 64VS A64VS
 fi
 
 #bash -l
